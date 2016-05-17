34003c784ff4a27470acdad12b9d5ccf14d74cb9
Moritz Juedes <mail@moritzjuedes.de>
2016-05-17 12:39:55 +0200

    Finishing off chapter 14: Form in View, Form Validation and more very good stuff (rewatch this one to understand forms)

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import SignUpForm
+
 
 def home(request):
 	title = "Willkommen!"
-	if request.user.is_authenticated():
-		title = """Hallo %s! %s, du bist echt cool!""" % (request.user, request.user)
+	form = SignUpForm(request.POST or None)
+
 	context = {
 		"template_title":	title,
 		"x":				3 + 4,
+		"form":				form,
 	}
+
+	if form.is_valid():
+		instance = form.save(commit=False) # This skips the saving in order to do custom stuff BEFORE saving it.
+		instance.save()
+		print (instance.email)
+		print (instance.timestamp)
+		context = { # Der Kontext wird hier verändert. Immer dran denken, das ist auch einfach nur ein variables Dictianary.
+		"template_title":	"Das hat geklappt. Danke!",
+		"x":				32 + 44,
+		}
+
+	if request.user.is_authenticated():
+		title = """Hallo %s! %s, du bist echt cool!""" % (request.user, request.user)
+	
+
 	return render(request, "home.html", context)
