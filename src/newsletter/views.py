572026d52731a06a47e6b7c5d5e167d409c7fa8b
Moritz Juedes <mail@moritzjuedes.de>
2016-05-17 11:59:27 +0200

    Finishing off chapter 13, View and template context (endlich hab ich das kapiert mit dem template context)

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-	return render(request, "home.html", {})
+	title = "Willkommen!"
+	if request.user.is_authenticated():
+		title = """Hallo %s! %s, du bist echt cool!""" % (request.user, request.user)
+	context = {
+		"template_title":	title,
+		"x":				3 + 4,
+	}
+	return render(request, "home.html", context)
