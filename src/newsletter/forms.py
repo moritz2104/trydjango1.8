524b0d41b74220b60f7d510a41abd16a700cae27
Moritz Juedes <mail@moritzjuedes.de>
2016-05-17 11:25:37 +0200

    Finishing off chapter 12, Form Validation (wir nehmen hier nur Leute, die Moritz heißen! :-))

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -6,3 +6,17 @@ class SignUpForm(forms.ModelForm):
 	class Meta:
 		model = SignUp
 		fields = ['full_name', 'email']
+
+	def clean_email(self): # WICHTIG: das hier muss so aussehen: clean_<field_name>(self):
+		email = self.cleaned_data.get('email') # Holt sich email nachdem es die django-eigene Vlidation durchlaufen hat
+		email_base, provider = email.split('@') # teilt email beim @
+		host, domain = provider.split('.') # teilt alles hinterm @ in vor und nach dem punkt
+		if not "moritz" in host:
+			raise forms.ValidationError("Ey, du brauchst eine Email-Adresse mit ...@moritz drin!")
+		return email
+
+	def clean_full_name(self):
+		full_name = self.cleaned_data.get('full_name')
+		if not "Moritz" in full_name:
+			raise forms.ValidationError("Tja, wir nehmen hier nur Leute, die Moritz heißen.")
+		return full_name
