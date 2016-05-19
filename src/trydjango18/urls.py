2ef7ce39ba7ccccfc4c7ce00857724d76520d5a2
Moritz Juedes <mail@moritzjuedes.de>
2016-05-19 10:14:16 +0200

    Chapter 18: Serving static files in development

--- src/trydjango18/urls.py
+++ src/trydjango18/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Examples:
@@ -7,3 +9,7 @@ urlpatterns = [
     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG: # += nimmt die variable, fügt etwas hinzu und setzt sie dann neu.
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
