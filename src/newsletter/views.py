e0a4959ec2d91f518ae7447cc8db0488b99d8a38
Moritz Juedes <mail@moritzjuedes.de>
2016-06-02 12:36:48 +0200

    Finishing trydjango1.8 tutorial

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render
 
 from .forms import ContactForm, SignUpForm
+from .models import SignUp
 # Create your views here.
 def home(request):
 	title = 'Welcome'
@@ -11,8 +12,6 @@ def home(request):
 		"title": title,
 		"form": form
 	}
-
-
 
 	if form.is_valid():
 		#form.save()
@@ -30,9 +29,16 @@ def home(request):
 			"title": "Thank you"
 		}
 
-
+	if request.user.is_authenticated() and request.user.is_staff:
+		# print (SignUp.objects.all())
+		for item in SignUp.objects.all():
+			print item
+		queryset = SignUp.objects.all().order_by('-timestamp')
+		context = {
+			'queryset': queryset,
+		}
+		
 	return render(request, "home.html", context)
-
 
 
 def contact(request):
